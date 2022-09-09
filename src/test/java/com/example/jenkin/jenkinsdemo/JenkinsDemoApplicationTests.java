314ded6d6083fe1fb73e3074e3e8b3682c9c9262
dipu11 <dipubuet2@gmail.com>
2022-09-09 22:35:22 +0600

    unit test with fail cases

--- src/test/java/com/example/jenkin/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/example/jenkin/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -20,13 +20,39 @@ class JenkinsDemoApplicationTests {
 	}
 
 	@Test
+	@Order(1)
 	public void testSuccess(){
-		Assertions.assertEquals(1, 1);
+		Assertions.assertEquals(1, 1, "should pass 2");
 	}
 
 	@Test
+	@Order(2)
 	public void testSuccess2(){
-		Assertions.assertEquals("test", "test", "should pass");
+		Assertions.assertEquals("test", "test", "should pass 2");
+	}
+
+	@Test
+	@Order(3)
+	public void testFail1(){
+		Assertions.assertEquals("test", "failthis", "should fail on 3");
+	}
+
+	@Test
+	@Order(4)
+	public void testSuccess3(){
+		Assertions.assertEquals("test", "test", "should pass on 4");
+	}
+
+	@Test
+	@Order(5)
+	public void testFail2(){
+		Assertions.assertEquals("test5", "test", "should fail on 5");
+	}
+
+	@Test
+	@Order(6)
+	public void testSuccess4(){
+		Assertions.assertNotEquals("test5", "test", "should success on 6");
 	}
 
 }
